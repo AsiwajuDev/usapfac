4968ca66d4305308368c271245e702c064d1acf5
asiwajudev <olanrewajuranda@gmail.com>
2020-08-25 22:33:10 +0100

    constants added

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,6 @@
 import Head from "next/head";
-import Container from "../components/container";
-import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
-import Intro from "../components/intro";
 import Layout from "../components/layout";
-import { getAllPostsForHome } from "../lib/api";
 import { SITE_NAME } from "../lib/constants";
-import Frame from "../components/frame";
 
 export default function Contact() {
   return (
